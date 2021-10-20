a6f61ad42f8cd4caaada6c2e022e6fe684a9d433
TanWaiHong <hongworkonly@gmail.com>
2021-10-20 13:17:29 +0800

    Add gunicorn

--- 100Days Project/69 training blog project/main.py
+++ 100Days Project/69 training blog project/main.py
@@ -6,8 +6,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import CreatePostForm
-from flask_gravatar import Gravatar
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from functools import wraps
 from flask import abort
